95b6b449c9cba8b5f9d14733056330676b210e2d
bu1th4nh <thanh10973@gmail.com>
2018-11-17 20:05:05 +0700

    Major updates for cp-template. Surprise in line 219\!.   #30yearsCTB

--- Utilities/cp_template/main.cpp
+++ Utilities/cp_template/main.cpp
@@ -5,6 +5,12 @@
 **                       | |_) | |_| | | |_| | | |__   _| | | | | | |                       **
 **                       |_.__/ \__,_|_|\__|_| |_|  |_| |_| |_|_| |_|                       **
 \*==========================================================================================*/
+//=====================================
+//Solution Briefing - Foreword
+
+
+
+//=====================================
 //Libraries and namespaces
 //#include <bits/stdc++.h>
 #include <algorithm>
@@ -14,6 +20,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cassert>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -36,26 +43,78 @@
 
 using namespace std;
 
+// #define DEBUG 
+// #define OPTIONAL_FEATURE
 
 //=====================================
 //Macroes
-#define sp " "
-#define el "\n"
+#define sp ' '
+#define el '\n'
 #define task ""
+#define maxinp ()
 #define fi first
 #define se second
 #define pb push_back
-#define maxinp (int)()
-#define siz(x) (int)(x.size())
-#define len(x) (int)(x.length())
-#define whole(x) x.begin(), x.end()
-#define FOR(i, x, y) for(int i=x; i<=y; ++i)
-#define FORl(i, x, y) for(int i=x; i<y; ++i)
-#define FORb(i, x, y) for(int i=x; i>=y; --i)
-#define FORlb(i, x, y) for(int i=x; i>y; --i)
+#define whole(x) x.begin(),x.end()
+#define whole_1(x) x.begin()+1,x.end()
+#define r_whole(x) x.rbegin(),x.rend()
+#define FOR(i, x, y) for(auto i=x; i<=y; ++i)
+#define FORl(i, x, y) for(auto i=x; i<y; ++i)
+#define FORb(i, x, y) for(auto i=x; i>=y; --i)
+#define FORlb(i, x, y) for(auto i=x; i>y; --i)
 #define MEMS(x, val) memset(x, val, sizeof(x))
-#define FILEOP() {freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout);}
-#define FILEOP_DEBUG() {freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); freopen(task".err", "w", stderr);}
+#define FILEOP()                        \
+{                                       \
+    freopen(task".inp", "r", stdin);    \
+    freopen(task".out", "w", stdout);   \
+}
+#define FILEOP_DEBUG()                  \
+{                                       \  
+    freopen(task".inp", "r", stdin);    \
+    freopen(task".out", "w", stdout);   \
+    freopen(task".err", "w", stderr);   \
+}
+
+//Macroes - Optional
+#ifdef OPTIONAL_FEATURE
+    #define pc(x) putchar(x)
+    #define gc() getchar()
+#endif
+
+
+
+//=====================================
+//Auxilary Functions
+#ifdef OPTIONAL_FEATURE
+    template<class T, class R> T max(T &x, R &y)
+    {
+        return x > y ? x : y;
+    }    
+    template<class T, class R> T min(T &x, R &y)
+    {
+        return x < y ? x : y;
+    }
+    template<class T, class R> void maximize(T &x, R y)
+    {
+        x = x > y ? x : y;
+    }    
+    template<class T, class R> void minimize(T &x, R y)
+    {
+        x = x < y ? x : y;
+    }
+    template<class T> int getBit(T &x, int i)
+    {
+        return ((x >> i) & 1) == 1;
+    }
+    template<class T> T __abs(T &x)
+    {
+        return (x = (x < 0) ? -x : x);
+    }
+#endif
+
+//=====================================
+//Constants
+
 
 
 //=====================================
@@ -75,19 +134,54 @@ typedef vector<vii> vvii;
 
 //=====================================
 //Functions and procedures
-//Initialization and preparation
+//File I/O and utilities
 void FileInit()
 {
     FILEOP()
 }
 void FileDebug()
 {
-    FILEOP_DEBUG()
+    #ifdef DEBUG
+        FILEOP_DEBUG()
+    #else
+        FILEOP()
+    #endif
 }
 void FileClose()
 {
     fclose(stdin);
     fclose(stdout);
+}
+template<class T> inline void read(T &ret)
+{
+    ret = T();
+    char c = 0;
+    bool neg = 0;
+
+    while(isdigit(c) == 0 && c != '-') c = getchar();
+    if(c == '-')
+    {
+        neg = 1;
+        c = getchar();
+    }
+
+    for(; isdigit(c) != 0; c = getchar()) ret = ret * 10 + c - '0';
+    ret = (neg) ? -ret : ret;
+
+    #ifdef DEBUG
+        cerr << "FastScan Diagnostic: " << ret << el;
+    #endif
+}
+template<class T> void print(T x)
+{
+    if(x < 0)
+    {
+        putchar('-');
+        x *= -1;
+    }
+
+    if(x > 9) print(x / 10);
+    putchar(x % 10 + '0');
 }
 
 //Enter
@@ -107,6 +201,7 @@ void Enter()
 
 
 
+
 //Output
 
 
@@ -119,3 +214,7 @@ int main()
     //Your code goes here
     return 0;
 }
+
+//=============================================================================//
+/**    CTB, you are always in my heart and in my code <3       #30yearsCTB    **/
+//=============================================================================//
